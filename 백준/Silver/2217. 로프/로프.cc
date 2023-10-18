2b7f22574df767e9bf64264eae2fa200460aced9
kichang_98 <143603126+skc-98@users.noreply.github.com>
2023-10-18 16:27:19 +0900

    [Silver IV] Title: 로프, Time: 16 ms, Memory: 2920 KB -BaekjoonHub

---
+++ 백준/Silver/2217. 로프/로프.cc
@@ -0,0 +1,34 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+bool cmp(int a, int b) {
+	return a > b;
+}
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+
+	int n;
+	cin >> n;
+	
+	vector<int> v;
+	for (int i = 0; i < n; i++) {
+		int a;
+		cin >> a;
+		v.push_back(a);
+	}
+	sort(v.begin(), v.end(), cmp);
+
+	int max = 0;
+	for (int i = 0; i < v.size(); i++) {
+		int w = (i + 1) * v[i];
+		if (max < w) max = w;
+	}
+	cout << max;
+
+	return 0;
+}
