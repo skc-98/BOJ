f956a936ca588fe9c062d7bd5b603d171480dfbb
kichang_98 <143603126+skc-98@users.noreply.github.com>
2023-10-19 13:43:32 +0900

    [Silver III] Title: 주유소, Time: 24 ms, Memory: 5220 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/13305. 주유소/주유소.cc
+++ 백준/Silver/13305. 주유소/주유소.cc
@@ -10,38 +10,26 @@ int main()
 	int n;
 	cin >> n;
 
-	vector<int> dis;
+	vector<long long> dis;
 	for (int i = 1; i <= n - 1; i++) {
 		int a;
 		cin >> a;
 		dis.push_back(a);
 	}
 
-	vector<int> money;
+	vector<long long> money;
 	for (int i = 1; i <= n; i++) {
 		int a;
 		cin >> a;
 		money.push_back(a);
 	}
 
-	int answer = 0;
-	int state = 0;
-	for (int i = 0; i < money.size() - 1; i++) {
-		int count = dis[i];
-		if (state != 0) {
-			state--;
-			continue;
-		}
-		for (int j = i + 1; j < money.size(); j++) {
-			if (money[j] <= money[i]) {
-				answer += (money[i] * count);
-				break;
-			}
-			else {
-				count += dis[j];
-				state++;
-			}
-		}
+	long long answer = 0;
+	
+	long long max = 999999999999;
+	for (int i = 0; i < dis.size(); i++) {
+		if (money[i] < max) max = money[i];
+		answer += max * dis[i];
 	}
 	cout << answer;
 	return 0;
