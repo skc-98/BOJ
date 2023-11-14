f778ede5df21b7a52cb3a5f9d5b8b5c27ff03757
kichang_98 <143603126+skc-98@users.noreply.github.com>
2023-11-14 18:36:07 +0900

    [Silver V] Title: 수 정렬하기 5, Time: 4764 ms, Memory: 8292 KB -BaekjoonHub

---
+++ 백준/Silver/15688. 수 정렬하기 5/수 정렬하기 5.cc
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+
+	int n;
+	cin >> n;
+
+	vector<int> v;
+	for (int i = 1; i <= n; i++) {
+		int a;
+		cin >> a;
+		v.push_back(a);
+	}
+	sort(v.begin(), v.end());
+	for (int i = 0; i < v.size(); i++) {
+		cout << v[i] << '\n';
+	}
+	return 0;
+}
