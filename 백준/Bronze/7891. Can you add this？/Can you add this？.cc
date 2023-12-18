416f16fe205aa6e0ad2b04352687da4bf70dfcfa
kichang_98 <143603126+skc-98@users.noreply.github.com>
2023-12-18 12:36:35 +0900

    [Bronze V] Title: Can you add this?, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/7891. Can you add this？/Can you add this？.cc
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+
+	int n;
+	long long a, b;
+
+	cin >> n;
+	for (int i = 1; i <= n; i++) {
+		cin >> a >> b;
+		cout << a + b << '\n';
+	}
+
+	return 0;
+}
