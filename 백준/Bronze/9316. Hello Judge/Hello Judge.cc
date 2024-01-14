fe969a51a90ec8878ed991f978a29707f8be3cba
kichang_98 <143603126+skc-98@users.noreply.github.com>
2024-01-14 20:36:04 +0900

    [Bronze IV] Title: Hello Judge, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/9316. Hello Judge/Hello Judge.cc
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	cin.tie(NULL); cout.tie(NULL);
+
+	int n;
+	cin >> n;
+
+	for (int i = 1; i <= n; i++) {
+		cout << "Hello World, Judge " << i << "!" << '\n';
+	}
+	return 0;
+}
