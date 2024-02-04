2042408880f1f02ae60b5a1e0aed40327ad51b05
kichang_98 <143603126+skc-98@users.noreply.github.com>
2024-02-04 20:26:06 +0900

    [Bronze V] Title: Plane, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/8370. Plane/Plane.cc
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+
+	int a, b, c, d;
+	cin >> a >> b >> c >> d;
+
+	cout << a * b + c * d;
+
+	return 0;
+}
