cf08b27d43d4fbcf8c5729bcfba442107977068f
kichang_98 <143603126+skc-98@users.noreply.github.com>
2024-03-29 15:23:30 +0900

    [Silver II] Title: 최대 힙, Time: 20 ms, Memory: 2916 KB -BaekjoonHub

---
+++ 백준/Silver/11279. 최대 힙/최대 힙.cc
@@ -0,0 +1,28 @@
+#include<iostream>
+#include<queue>
+using namespace std;
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+
+	int n;
+	cin >> n;
+
+	priority_queue<int> q;
+	for (int i = 0; i < n; i++) {
+		int a;
+		cin >> a;
+		if (a == 0) {
+			if (q.empty()) cout << 0 << '\n';
+			else {
+				cout << q.top() << '\n';
+				q.pop();
+			}
+			continue;
+		}
+		q.push(a);
+	}
+	return 0;
+}
