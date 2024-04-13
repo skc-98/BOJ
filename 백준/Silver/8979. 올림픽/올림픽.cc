0cf076b0f6bddff2716d75041a63e4b4688c2fdc
kichang_98 <143603126+skc-98@users.noreply.github.com>
2024-04-13 16:10:20 +0900

    [Silver V] Title: 올림픽, Time: 0 ms, Memory: 2160 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/8979. 올림픽/올림픽.cc
+++ 백준/Silver/8979. 올림픽/올림픽.cc
@@ -1,48 +1,55 @@
 #include<iostream>
+#include<vector>
+#include<algorithm>
 using namespace std;
 
-int gold[1001];
-int silver[1001];
-int bronze[1001];
-int crank[1001] = { 0, };
+bool cmp(const pair<int, vector<int>>& a, const pair<int, vector<int>>& b) {
+	if (a.second[0] == b.second[0]) {
+		if (a.second[1] == b.second[1]) {
+			return a.second[2] > b.second[2];
+		}
+		return a.second[1] > b.second[1];
+	}
+	return a.second[0] > b.second[0];
+}
 
 int main()
 {
-	int n, m, i, j;
-	int a, b, c, d;
-	int temp;
+	ios::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+
+	int n, m;
 	cin >> n >> m;
-	for (i = 1; i <= n; i++) {
-		cin >> a;
-		cin >> gold[a] >> silver[a] >> bronze[a];
-		crank[i] = i;
-	}
-	for (i = 1; i <= n; i++) {
-		for (j = i; j <= n; j++) {
-			if (gold[j] > gold[i]) {
-				temp = crank[i];
-				crank[i] = crank[j];
-				crank[j] = temp;
-			}
-			if (gold[j] == gold[i] && silver[j] > silver[i]) {
-				temp = crank[i];
-				crank[i] = crank[j];
-				crank[j] = temp;
-			}
-			if (gold[j] == gold[i] && silver[j] == silver[i] && bronze[j]>bronze[i]) {
-				temp = crank[i];
-				crank[i] = crank[j];
-				crank[j] = temp;
-			}
+
+	vector<pair<int, vector<int>>> v;
+	for (int i = 0; i < n; i++) {
+		int c;
+		cin >> c;
+		v.push_back(make_pair(c, vector<int>()));
+		for (int j = 0; j < 3; j++) {
+			int a;
+			cin >> a;
+			v[i].second.push_back(a);
 		}
 	}
-	for (i = 1; i <= n; i++) {
-		for (j = i; j <= n; j++) {
-			if (gold[j] == gold[i] && silver[j] == silver[i] && bronze[j] == bronze[i]) {
-				if (crank[i] < crank[j]) crank[j] = crank[i];
-			}
+	sort(v.begin(), v.end(), cmp);
+
+	vector<int> rank(n, 1);
+	for (int i = 1; i < n; i++) {
+		if (v[i].second == v[i - 1].second) {
+			rank[i] = rank[i - 1];
+		}
+		else {
+			rank[i] = i + 1;
 		}
 	}
-	cout << crank[m];
+
+
+	for (int i = 0; i < n; i++) {
+		if (v[i].first == m) {
+			cout << rank[i];
+			break;
+		}
+	}
 	return 0;
 }
