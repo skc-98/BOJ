7354eea60307cb012b79f2f2d929bd22d50aa04c
kichang_98 <143603126+skc-98@users.noreply.github.com>
2024-05-10 16:31:14 +0900

    [Silver I] Title: IOIOI, Time: 12 ms, Memory: 3680 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/5525. IOIOI/IOIOI.cc
+++ 백준/Silver/5525. IOIOI/IOIOI.cc
@@ -5,38 +5,28 @@ using namespace std;
 int main()
 {
 	ios::sync_with_stdio(false);
-	cin.tie(NULL);
+	cin.tie(NULL); cout.tie(NULL);
 
 	int n, m;
+	string s;
 	cin >> n;
 	cin >> m;
-	string s;
 	cin >> s;
 
-	string ioi = "I";
-	for (int i = 1; i <= n; i++) {
-		ioi += "OI";
-	}
-
 	int ans = 0;
-	for (int i = 0; i < s.length(); i++) {
-		int state = 0;
-		int count = 0;
-		if (s[i] == 'I') {
-			for (int j = i; j < i + ioi.length(); j++) {
-				if (s[j] == ioi[count]) count++;
-				else {
-					state = 1;
-					break;
+	for (int i = 0; i < m; i++) {
+		if (s[i + 1] == 'O' && s[i + 2] == 'I') {
+			int tmp = 0;
+			while (s[i] == 'I' && s[i + 1] == 'O') {
+				i += 2;
+				tmp++;
+				if (s[i] == 'I' && tmp == n) {
+					ans++;
+					tmp--;
 				}
 			}
 		}
-		if (s[i] != 'I') state = 1;
-		if (state == 0) {
-			ans++;
-		}
 	}
-
 	cout << ans;
 	return 0;
 }
