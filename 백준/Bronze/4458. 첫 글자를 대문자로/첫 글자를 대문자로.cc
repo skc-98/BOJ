83525b0c81eed9dd8b89bc7e27f8f2fb7072dcfc
kichang_98 <143603126+skc-98@users.noreply.github.com>
2024-07-05 22:00:57 +0900

    [Bronze III] Title: 첫 글자를 대문자로, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/4458. 첫 글자를 대문자로/첫 글자를 대문자로.cc
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+
+	int n;
+	cin >> n;
+
+	cin.ignore();
+	for (int i = 0; i < n; i++) {
+		string s;
+		getline(cin, s);
+		if(s[0]>='a' && s[0]<='z') s[0] -= 32;
+		cout << s << '\n';
+	}
+
+	return 0;
+}
