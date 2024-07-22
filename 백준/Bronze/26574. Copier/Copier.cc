ba92710c580fab0be8bab984ece666a7b4dd7089
kichang_98 <143603126+skc-98@users.noreply.github.com>
2024-07-22 13:45:41 +0900

    [Bronze V] Title: Copier, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/26574. Copier/Copier.cc
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+
+	int n;
+	cin >> n;
+
+	for (int i = 0; i < n; i++) {
+		int a;
+		cin >> a;
+		cout << a << " " << a << '\n';
+	}
+
+	return 0;
+}
