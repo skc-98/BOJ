a9e5ef17f1a666bb59b7ea963e2d83d580a02fde
kichang_98 <143603126+skc-98@users.noreply.github.com>
2024-07-24 08:25:27 +0900

    [Bronze V] Title: 果物 (Fruit), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/31606. 果物 （Fruit）/果物 （Fruit）.cc
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+
+	int n, m;
+	cin >> n >> m;
+	cout << n + m + 3;
+
+	return 0;
+}
