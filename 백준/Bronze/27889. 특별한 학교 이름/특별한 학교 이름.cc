e530043b4a7e0a86b4831ba545c7d878cc9f336d
kichang_98 <143603126+skc-98@users.noreply.github.com>
2024-07-31 19:05:42 +0900

    [Bronze V] Title: 특별한 학교 이름, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/27889. 특별한 학교 이름/특별한 학교 이름.cc
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+
+	string s;
+	cin >> s;
+
+	if (s == "NLCS") cout << "North London Collegiate School";
+	else if (s == "BHA") cout << "Branksome Hall Asia";
+	else if (s == "KIS") cout << "Korea International School";
+	else if (s == "SJA") cout << "St. Johnsbury Academy";
+	return 0;
+}
