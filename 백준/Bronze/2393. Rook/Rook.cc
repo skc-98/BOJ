234f0ae38cbe49033f3944c4ac151144e002e168
kichang_98 <richkc2006@naver.com>
2024-10-23 17:29:38 +0900

    [Bronze V] Title: Rook, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2393. Rook/Rook.cc
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+
+	cout << "  ___  ___  ___" << '\n';
+	cout << "  | |__| |__| |" << '\n';
+	cout << "  |           |" << '\n';
+	cout << "   \\_________/" << '\n';
+	cout << "    \\_______/" << '\n';
+	cout << "     |     |" << '\n';
+	cout << "     |     |" << '\n';
+	cout << "     |     |" << '\n';
+	cout << "     |     |" << '\n';
+	cout << "     |_____|" << '\n';
+	cout << "  __/       \\__" << '\n';
+	cout << " /             \\" << '\n';
+	cout << "/_______________\\" << '\n';
+	cout << "";
+
+
+	return 0;
+}
