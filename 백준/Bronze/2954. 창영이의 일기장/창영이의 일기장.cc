ec1a8a395da66307b653780c5c2da5adafc21246
kichang_98 <richkc2006@naver.com>
2025-01-01 21:52:38 +0900

    [Bronze II] Title: 창영이의 일기장, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/2954. 창영이의 일기장/창영이의 일기장.cc
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+
+	string s;
+	getline(cin, s);
+
+	string ans = "";
+	for (int i = 0; i < s.length(); i++) {
+		if (s[i] == 'a' || s[i] == 'e' || s[i] == 'i' || s[i] == 'o' || s[i] == 'u') {
+			ans += s[i];
+			i += 2;
+		}
+		else ans += s[i];
+	}
+	cout << ans;
+	return 0;
+}
