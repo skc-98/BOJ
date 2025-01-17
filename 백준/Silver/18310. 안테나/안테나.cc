f0f433ce7a710d928305af191070e0e65d1efdee
kichang_98 <richkc2006@naver.com>
2025-01-17 23:00:31 +0900

    [Silver III] Title: 안테나, Time: 32 ms, Memory: 2804 KB -BaekjoonHub

---
+++ 백준/Silver/18310. 안테나/안테나.cc
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+
+	int n;
+	cin >> n;
+	vector<int> v(n);
+	for (int i = 0; i < n; i++) {
+		cin >> v[i];
+	}
+	sort(v.begin(), v.end());
+	cout << v[(n - 1) / 2];
+	return 0;
+}
