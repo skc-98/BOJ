155b80a127ef167590f96a180e324f35c74a3159
kichang_98 <richkc2006@naver.com>
2025-03-03 20:55:05 +0900

    [Silver III] Title: N과 M (1), Time: 1768 ms, Memory: 68136 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.java
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -1,40 +1,41 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
 public class Main {
-
-	static int n, m;
-	static List<Integer> li = new ArrayList<>();
-	static boolean[] visit;
-
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws Exception {
+		// 빠른 입력을 위한 BufferedReader 사용
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
+		// n과 m 입력
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		n = Integer.parseInt(st.nextToken());
 		m = Integer.parseInt(st.nextToken());
 
 		visit = new boolean[n + 1];
-		func(0);
+		backTracking(0);
 	}
 
-	public static void func(int num) {
-		if (num == m) {
-			for (int iter : li) {
-				System.out.print(iter + " ");
+	static int n, m;
+	static List<Integer> numbers = new ArrayList<>();
+	static boolean[] visit;
+
+	static void backTracking(int cnt) {
+		if (numbers.size() == m) {
+			for (int i = 0; i < numbers.size(); i++) {
+				System.out.print(numbers.get(i) + " ");
 			}
 			System.out.println();
 			return;
 		}
 
 		for (int i = 1; i <= n; i++) {
-			if (!visit[i]) {
-				visit[i] = true;
-				li.add(i);
-				func(num + 1);
-				li.remove(li.size() - 1);
-				visit[i] = false;
-			}
+			if (visit[i])
+				continue;
+			visit[i] = true;
+			numbers.add(i);
+			backTracking(cnt);
+			numbers.remove(numbers.size() - 1);
+			visit[i] = false;
 		}
 	}
 }
