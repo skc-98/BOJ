da954ebed213bb0139722fb10fc07d4de22eda79
kichang_98 <richkc2006@naver.com>
2025-03-08 18:35:36 +0900

    [Bronze IV] Title: 타일 채우기 4, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15700. 타일 채우기 4/타일 채우기 4.cc
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    cin.tie(NULL); cout.tie(NULL);
+    long long n,m;
+    cin >>n>>m;
+    
+    long long value=n*m;
+    cout << value/2;
+    return 0;
+}
