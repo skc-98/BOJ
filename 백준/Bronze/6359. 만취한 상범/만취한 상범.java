8fe854579c99d29c63841f27961d2e7e0bab8655
kichang_98 <richkc2006@naver.com>
2025-03-28 23:48:59 +0900

    [Bronze II] Title: 만취한 상범, Time: 104 ms, Memory: 14020 KB -BaekjoonHub

--- 백준/Bronze/6359. 만취한 상범/만취한 상범.java
+++ 백준/Bronze/6359. 만취한 상범/만취한 상범.java
@@ -3,7 +3,6 @@ import java.io.*;
 
 public class Main {
 	public static void main(String[] args) throws Exception {
-
 		// BufferedReader 사용
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
