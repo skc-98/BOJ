6d09a905630e118c1665e9762c9d9ca1e104ede3
kichang_98 <richkc2006@naver.com>
2025-04-20 15:39:37 +0900

    [Gold V] Title: 사과나무, Time: 344 ms, Memory: 23600 KB -BaekjoonHub

--- 백준/Gold/19539. 사과나무/사과나무.java
+++ 백준/Gold/19539. 사과나무/사과나무.java
@@ -2,29 +2,45 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws Exception {
+		// BufferedReader 사용
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
+		// 사과나무의 수 입력
 		int n = Integer.parseInt(br.readLine());
 
-		int cnt1 = 0, cnt2 = 0;
+		// 1주는 횟수, 2주는 횟수 카운트
+		int oneCount = 0;
+		int twoCount = 0;
+
+		// 각 나무의 정보 입력
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		for (int i = 0; i < n; i++) {
-			int num = Integer.parseInt(st.nextToken());
-			cnt2 += num / 2;
-			cnt1 += num % 2;
+			// 나무 입력
+			int t = Integer.parseInt(st.nextToken());
+
+			// 나무 높이를 2로 나눈 몫은 2주는 횟수, 나머지는 1주는 횟수로 카운트
+			oneCount += (t % 2);
+			twoCount += (t / 2);
 		}
 
+		// 이제 2씩 주는 횟수를 감소하면서 1씩 주는 횟수 2만큼 증가 반복
 		while (true) {
-			if (cnt2 < cnt1) {
-				System.out.println("NO");
-				return;
-			} else if (cnt2 == cnt1) {
-				System.out.println("YES");
-				return;
+			// 1씩 주는 날이 더 커져버리면 불가능한 경우
+			if (oneCount > twoCount) {
+				System.out.print("NO");
+				break;
 			}
-			cnt2--;
-			cnt1 += 2;
+
+			// 같아지면 가능한 경우임
+			else if (oneCount == twoCount) {
+				System.out.print("YES");
+				break;
+			}
+
+			// twoCount는 1 감소, oneCount는 2증가
+			twoCount--;
+			oneCount += 2;
 		}
 	}
 }
