67e05972e8e222528b4c3c804267ec4bf231508d
kichang_98 <richkc2006@naver.com>
2025-09-03 13:41:11 +0900

    [Bronze III] Title: 스타후르츠, Time: 108 ms, Memory: 14192 KB -BaekjoonHub

---
+++ 백준/Bronze/17496. 스타후르츠/스타후르츠.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+	public static void main(String[] args) throws Exception {
+		// BufferedReader 사용
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		// 입력
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int n = Integer.parseInt(st.nextToken());
+		int t = Integer.parseInt(st.nextToken());
+		int c = Integer.parseInt(st.nextToken());
+		int p = Integer.parseInt(st.nextToken());
+
+		int harvests = (n - 1) / t;
+		System.out.println(harvests * c * p);
+	}
+}
