5aca27b5a7d194e54380c4bf8357edb27e98eb6f
kichang_98 <richkc2006@naver.com>
2025-10-23 15:13:36 +0900

    [level 1] Title: 어린 동물 찾기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/1/59037. 어린 동물 찾기/어린 동물 찾기.sql
+++ 프로그래머스/1/59037. 어린 동물 찾기/어린 동물 찾기.sql
@@ -1,3 +1,4 @@
-select animal_id, name from animal_ins
-where intake_condition!='Aged'
-order by animal_id;
+select animal_id, name
+from animal_ins
+where INTAKE_CONDITION not like "%Aged%"
+order by animal_id asc;
