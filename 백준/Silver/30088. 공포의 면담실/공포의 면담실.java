b8701d98dd4b71a0420b52793683be675c490bdf
kichang_98 <richkc2006@naver.com>
2025-12-01 06:29:28 +0900

    [Silver V] Title: 공포의 면담실, Time: 580 ms, Memory: 82632 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/30088. 공포의 면담실/공포의 면담실.java
+++ 백준/Silver/30088. 공포의 면담실/공포의 면담실.java
@@ -8,36 +8,24 @@ public class Main {
 
 		// 부서의 수, 각 부서 입력
 		int n = Integer.parseInt(br.readLine());
-		List<List<Integer>> li = new ArrayList<>();
+		long[] d = new long[n];
 		for (int i = 0; i < n; i++) {
-			li.add(new ArrayList<>());
 			StringTokenizer st = new StringTokenizer(br.readLine());
 			int people = Integer.parseInt(st.nextToken());
 			for (int j = 0; j < people; j++) {
-				li.get(i).add(Integer.parseInt(st.nextToken()));
+				int num = Integer.parseInt(st.nextToken());
+				d[i] += num;
 			}
 		}
 
 		// 각 부서 면담시간 합으로 정렬
-		Collections.sort(li, (a, b) -> {
-			int sumA = 0;
-			for (int x : a)
-				sumA += x;
-			int sumB = 0;
-			for (int x : b)
-				sumB += x;
-			return sumA - sumB;
-		});
+		Arrays.sort(d);
 
 		// 각 부서 합 시간 누적해서 더하기
-		int ans = 0;
-		int total = 0;
-		for (int i = 0; i < li.size(); i++) {
-			int sum = 0;
-			for (int j = 0; j < li.get(i).size(); j++) {
-				sum += li.get(i).get(j);
-			}
-			total += sum;
+		long ans = 0;
+		long total = 0;
+		for (int i = 0; i < n; i++) {
+			total += d[i];
 			ans += total;
 		}
 
