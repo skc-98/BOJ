16361071764d371b27b846306b004d2026384601
kichang_98 <richkc2006@naver.com>
2026-01-12 14:56:58 +0900

    [Silver II] Title: 알고리즘 수업 - 너비 우선 탐색 2, Time: 1492 ms, Memory: 101232 KB -BaekjoonHub

---
+++ 백준/Silver/24445. 알고리즘 수업 － 너비 우선 탐색 2/알고리즘 수업 － 너비 우선 탐색 2.java
@@ -0,0 +1,74 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+	// 전역 변수 선언부
+	static int n, m, start;
+	static List<List<Integer>> li;
+	static int[] visit;
+	static int count = 1;
+
+	public static void main(String[] args) throws Exception {
+		// 빠른 입력을 위한 BufferedReader 사용
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		// 정점의 수, 간선의 수, 시작 정점
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		n = Integer.parseInt(st.nextToken());
+		m = Integer.parseInt(st.nextToken());
+		start = Integer.parseInt(st.nextToken());
+
+		// 간선 정보 담을 리스트 생성
+		li = new ArrayList<>();
+		for (int i = 0; i <= n; i++) {
+			li.add(new ArrayList<>());
+		}
+
+		// 각 간선의 정보 입력
+		for (int i = 0; i < m; i++) {
+			st = new StringTokenizer(br.readLine());
+			int from = Integer.parseInt(st.nextToken());
+			int to = Integer.parseInt(st.nextToken());
+			li.get(from).add(to);
+			li.get(to).add(from);
+		}
+
+		// 간선 정렬
+		for (int i = 1; i <= n; i++) {
+			li.get(i).sort(Collections.reverseOrder());
+		}
+
+		// BFS 시작
+		visit = new int[n + 1];
+		Arrays.fill(visit, -1);
+		bfs(start);
+
+		// 출력하기
+		for (int i = 1; i <= n; i++) {
+			if (visit[i] == -1) {
+				System.out.println(0);
+				continue;
+			}
+			System.out.println(visit[i]);
+		}
+	}
+
+	// BFS 메서드
+	static void bfs(int start) {
+		Queue<Integer> q = new ArrayDeque<>();
+		q.add(start);
+		visit[start] = count;
+
+		while (!q.isEmpty()) {
+			int now = q.poll();
+
+			for (int next : li.get(now)) {
+				if (visit[next] != -1)
+					continue;
+				count++;
+				visit[next] = count;
+				q.add(next);
+			}
+		}
+	}
+}
