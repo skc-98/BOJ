7931e79cf53f0c9575b8122a87cb5f2a525effae
kichang_98 <richkc2006@naver.com>
2026-03-30 19:35:20 +0900

    [Bronze III] Title: 골뱅이 찍기 - 돌아간 ㅈ, Time: 1104 ms, Memory: 29132 KB -BaekjoonHub

---
+++ 백준/Bronze/23809. 골뱅이 찍기 － 돌아간 ㅈ/골뱅이 찍기 － 돌아간 ㅈ.java
@@ -0,0 +1,68 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+	public static void main(String[] args) throws Exception {
+		// 빠른 입력을 위한 BufferedReader 사용
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		// n 입력
+		int n = Integer.parseInt(br.readLine());
+
+		// 골뱅이 찍기
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n; j++) {
+				System.out.print("@");
+			}
+			for (int j = 0; j < n * 3; j++) {
+				System.out.print(" ");
+			}
+			for (int j = 0; j < n; j++) {
+				System.out.print("@");
+			}
+			System.out.println();
+		}
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n; j++) {
+				System.out.print("@");
+			}
+			for (int j = 0; j < n * 2; j++) {
+				System.out.print(" ");
+			}
+			for (int j = 0; j < n; j++) {
+				System.out.print("@");
+			}
+			System.out.println();
+		}
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n * 3; j++) {
+				System.out.print("@");
+			}
+			System.out.println();
+		}
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n; j++) {
+				System.out.print("@");
+			}
+			for (int j = 0; j < n * 2; j++) {
+				System.out.print(" ");
+			}
+			for (int j = 0; j < n; j++) {
+				System.out.print("@");
+			}
+			System.out.println();
+		}
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n; j++) {
+				System.out.print("@");
+			}
+			for (int j = 0; j < n * 3; j++) {
+				System.out.print(" ");
+			}
+			for (int j = 0; j < n; j++) {
+				System.out.print("@");
+			}
+			System.out.println();
+		}
+	}
+}
