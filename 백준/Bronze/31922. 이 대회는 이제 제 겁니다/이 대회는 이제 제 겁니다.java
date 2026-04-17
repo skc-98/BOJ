92c6214aed9649e80e26e33324af2b6e1726a90d
kichang_98 <richkc2006@naver.com>
2026-04-18 07:12:15 +0900

    [Bronze IV] Title: 이 대회는 이제 제 겁니다, Time: 112 ms, Memory: 14188 KB -BaekjoonHub

---
+++ 백준/Bronze/31922. 이 대회는 이제 제 겁니다/이 대회는 이제 제 겁니다.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+	public static void main(String[] args) throws Exception {
+		// 빠른 입력을 위한 BufferedReader 사용
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		// 대회 우승 상금
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int a = Integer.parseInt(st.nextToken());
+		int b = Integer.parseInt(st.nextToken());
+		int c = Integer.parseInt(st.nextToken());
+		
+		// 답 출력하기
+		System.out.println(Math.max(a + c, b));
+	}
+}
