7f653060c16319c83b7f1aa47ff0e3d124c0265d
kichang_98 <richkc2006@naver.com>
2026-09-18 02:27:07 +0900

    [level 1] Title: 덧칠하기, Time: 0.91 ms, Memory: 76.9 MB -BaekjoonHub

---
+++ 프로그래머스/1/161989. 덧칠하기/덧칠하기.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int solution(int n, int m, int[] section) {
+        int answer = 0;
+        int p = 0;
+
+        for (int i = 0; i < section.length; i++) {
+            if (section[i] > p) {
+                answer++;
+                p = section[i] + m - 1;
+            }
+        }
+
+        return answer;
+    }
+}
