97fe673b982135e63df6c9301dc71047a503d5c7
kichang_98 <richkc2006@naver.com>
2026-09-26 04:39:37 +0900

    [level 2] Title: 업그레이드 된 아이템 구하기, Time: , Memory: undefined -BaekjoonHub

---
+++ 프로그래머스/2/273711. 업그레이드 된 아이템 구하기/업그레이드 된 아이템 구하기.sql
@@ -0,0 +1,10 @@
+select i.item_id, i.item_name, i.rarity
+from item_info i
+join item_tree t
+on i.item_id = t.item_id
+where t.parent_item_id in (
+    select item_id
+    from item_info
+    where rarity = 'RARE'
+)
+order by i.item_id desc;
